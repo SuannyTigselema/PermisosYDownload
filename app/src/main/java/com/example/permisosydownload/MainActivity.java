62b9eab1e33fdd939efe93fbc21db90dd02376a0
SuannyTigselema <66803060+SuannyTigselema@users.noreply.github.com>
2020-08-06 22:18:16 -0500

    descargar archivo

--- app/src/main/java/com/example/permisosydownload/MainActivity.java
+++ app/src/main/java/com/example/permisosydownload/MainActivity.java
@@ -15,6 +15,8 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -30,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private final static int RESIZE_PHOTO_PIXELS_PERCENTAGE=50;
     private MagicalCamera magicalCamera;
     private ImageView imageViewFoto;
+    private String path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,36 +43,36 @@ public class MainActivity extends AppCompatActivity {
        // findViewById(R.id.btnCamara).setOnClickListener(this);
         imageViewFoto=findViewById(R.id.imageView);
 
-       /*ArrayList<String> permisos = new ArrayList<String>();
+       ArrayList<String> permisos = new ArrayList<String>();
         permisos.add(Manifest.permission.CAMERA);
         permisos.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
         permisos.add(Manifest.permission.READ_EXTERNAL_STORAGE);
         permisos.add(Manifest.permission.WRITE_CALENDAR);
 
-        getPermission(permisos);*/
+        getPermission(permisos);
 
-        String[] permissions = new String[]{
+     /*   String[] permissions = new String[]{
                 Manifest.permission.CAMERA,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 Manifest.permission.READ_EXTERNAL_STORAGE
         };
-       // getPermission(permisos);
-        magicalPermissions = new MagicalPermissions(this, permissions);
+        getPermission(permisos);*/
+        magicalPermissions = new MagicalPermissions(this, permisos.toArray(new String[permisos.size()]));
         magicalCamera = new MagicalCamera(this, RESIZE_PHOTO_PIXELS_PERCENTAGE,magicalPermissions);
-
     }
 
     public void onClick(View v)
     {
         magicalCamera.takePhoto();
     }
+
     public void getPermission(ArrayList<String> permisosSolicitados){
         ArrayList<String> listPermisosNOAprob = getPermisosNoAprobados(permisosSolicitados);
         if (listPermisosNOAprob.size()>0)
             if (Build.VERSION.SDK_INT >= 23)
                 requestPermissions(listPermisosNOAprob.toArray(new String[listPermisosNOAprob.size()]), 1);
-
     }
+
     public ArrayList<String> getPermisosNoAprobados(ArrayList<String>  listaPermisos) {
         ArrayList<String> list = new ArrayList<String>();
         for(String permiso: listaPermisos) {
@@ -80,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return list;
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -94,13 +98,6 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this.getApplicationContext(), s, Toast.LENGTH_LONG).show();
         }
     }
-    public void MostrarDescargas(View view){
-
-        Intent intent = new Intent();
-        intent.setAction(DownloadManager.ACTION_VIEW_DOWNLOADS);
-        startActivity(intent);
-
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -109,32 +106,42 @@ public class MainActivity extends AppCompatActivity {
         {
             magicalCamera.resultPhoto(requestCode,resultCode,data);
             imageViewFoto.setImageBitmap(magicalCamera.getPhoto());
+        }
 
+        switch(requestCode) {
+            case 10:
+                //path= data.getDataString();
+                Uri uri = data.getData();
+                Uri docUri = DocumentsContract.buildDocumentUriUsingTree(uri,
+                        DocumentsContract.getTreeDocumentId(uri));
+                path = uri.getPath();
+                Toast.makeText(this.getApplicationContext(),"RUTA: "  + path,Toast.LENGTH_LONG).show();
+
+                String url = "http://tierra.rediris.es/hidrored/ebooks/miguel/AguaFuenteVida.pdf";
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                request.setDescription("PDF");
+                request.setTitle("Pdf");
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                    request.allowScanningByMediaScanner();
+                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                }
+                request.setDestinationInExternalPublicDir(path, "filedownload.pdf");
+                DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                try {
+                    manager.enqueue(request);
+                } catch (Exception e) {
+                    Toast.makeText(this.getApplicationContext(),"Error: "  + e.getMessage(),Toast.LENGTH_LONG).show();
+                }
+
+                break;
         }
     }
 
     public void BajarDoc(View view){
         Intent myFile;
-        myFile = new Intent(Intent.ACTION_GET_CONTENT);
-        myFile.setType("file/*");
-        startActivityForResult(myFile,10);
-
-
-        String url = "https://www.uteq.edu.ec/revistacyt/archivositio/instrucciones_arbitros.pdf";
-        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-        request.setDescription("PDF");
-        request.setTitle("Pdf");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            request.allowScanningByMediaScanner();
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-        }
-        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "filedownload.pdf");
-        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-        try {
-            manager.enqueue(request);
-        } catch (Exception e) {
-            Toast.makeText(this.getApplicationContext(),"Error: "  + e.getMessage(),Toast.LENGTH_LONG).show();
-        }
+        myFile = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+        myFile.addCategory(Intent.CATEGORY_DEFAULT);
+        startActivityForResult(Intent.createChooser(myFile, "Elija un directorio"),10);
 
     }
 
